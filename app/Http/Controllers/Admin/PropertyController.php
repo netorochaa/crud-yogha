39ee724e12efc861a46c520ede550a57d8c27963
netorochaa <jrr.neto@hotmail.com>
2021-06-05 15:23:18 -0300

    Configured factory tests

--- app/Http/Controllers/Admin/PropertyController.php
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -3,26 +3,26 @@
 namespace Database\Factories;
 
 use App\Models\Property;
+use App\Models\Type;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PropertyFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Property::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
-            //
+            'name'      => $this->faker->sentence(), 
+            'city'      => $this->faker->city(), 
+            'occupant'  => $this->faker->numberBetween(1, 99), 
+            'bedroom'   => $this->faker->numberBetween(1, 10), 
+            'restroom'  => $this->faker->numberBetween(1, 10), 
+            'internet'  => $this->faker->numberBetween(0, 1), 
+            'pool'      => $this->faker->numberBetween(0, 1), 
+            'type_id'   => function() {
+                return Type::factory()->create();
+            },
         ];
     }
 }
