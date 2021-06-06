fbe360caa6d1b5a714afb1e89886447b9d9565bc
netorochaa <jrr.neto@hotmail.com>
2021-06-06 15:16:53 -0300

    Implemented type controller, validations and create and listing pages

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -5,36 +5,50 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Type;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreTypeRequest;
+use App\Http\Requests\UpdateTypeRequest;
 
 class TypeController extends Controller
 {
     public function index()
     {
-        //
+        return view('admin.type.index', [
+            'types' => Type::all()->sortBy('id')
+        ]);
     }
 
-    public function store(Request $request)
+    public function store(StoreTypeRequest $request)
     {
-        //
-    }
+        Type::create($request->all());
 
-    public function show(Type $type)
-    {
-        //
+        session()->flash('success', 'Tipo de propriedade cadastrado!');
+        
+        return redirect()->route('type.index');
     }
 
     public function edit(Type $type)
     {
-        //
+        return view('admin.property.index', [
+            'types' => Type::all()->sortBy('id'),
+            'property'  => $type
+        ]);
     }
 
-    public function update(Request $request, Type $type)
+    public function update(UpdateTypeRequest $request, Type $type)
     {
-        //
+        $type->update(['name' => $request->name]);
+        
+        session()->flash('success', 'Tipo de propriedade ' . $type->id . ' - ' . $type->name . ' atualizado!');
+
+        return redirect()->route('type.index');
     }
 
     public function destroy(Type $type)
     {
-        //
+        $type->delete();
+
+        session()->flash('success', 'Tipo de propriedade removido!');
+
+        return redirect()->route('type.index');
     }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -11,6 +11,10 @@ class Type extends Model
     use HasFactory, SoftDeletes;
     protected $fillable = ['name'];
 
+    public const VALIDATION_RULES = [
+        'name' => 'required|max:255'  
+    ];
+
     public function properties()
     {
         return $this->hasMany(Property::class);
