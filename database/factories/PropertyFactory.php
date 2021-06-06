772d535d257ee5cf1f57ffa9ded560298f4f1fe6
netorochaa <jrr.neto@hotmail.com>
2021-06-06 16:34:20 -0300

    Fixes on fake tests

--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -13,13 +13,13 @@ class PropertyFactory extends Factory
     public function definition()
     {
         return [
-            'name'      => $this->faker->sentence(), 
+            'name'      => $this->faker->text($maxNbChars = 50), 
             'city'      => $this->faker->city(), 
             'occupant'  => $this->faker->numberBetween(1, 99), 
             'bedroom'   => $this->faker->numberBetween(1, 10), 
             'restroom'  => $this->faker->numberBetween(1, 10), 
-            'internet'  => $this->faker->numberBetween(0, 1), 
-            'pool'      => $this->faker->numberBetween(0, 1), 
+            'internet'  => $this->faker->boolean() ? 'on' : null, 
+            'pool'      => $this->faker->boolean() ? 'on' : null, 
             'type_id'   => function() {
                 return Type::factory()->create();
             },
